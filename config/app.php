8e718f9450c241db9d6bf88c0ccaf8ef6b5ac8c2
kessalhi <khalid.essalhi@sofrecom.com>
2021-09-01 22:18:11 +0100

    local changes

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://ixiir.com/ixiirnewfresh'),
+    'url' => env('APP_URL', 'https://rec.ixiir.com/ixiirnewfresh'),
 
     'asset_url' => env('ASSET_URL', null),
 
